4d99b30b6e464304ec3645b2a7778b718f2b4529
AAAEMPIRE <aaa@aaaiinvst.com>
2024-08-22 05:06:11 -0700

    Created using Colab

---
+++ task_automation.py
@@ -0,0 +1,50 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "name": "task_automation.py",
+      "authorship_tag": "ABX9TyPIX+EO+JHv9Vbe1CTrX4Qf",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/AAAEMPIRE/-HOME-save_and_organize_history.sh-crontab-/blob/main/task_automation.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "G3mdDMH37G60"
+      },
+      "outputs": [],
+      "source": []
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "# Necessary Imports with open('/content/drive/MyDrive/TaskAutomation/new_task_automation.py', 'w') as f:\n",
+        "    f.write(script_content)"
+      ],
+      "metadata": {
+        "id": "AmsVBOb47gXo"
+      }
+    }
+  ]
+}
